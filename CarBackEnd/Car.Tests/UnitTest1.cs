b5a6fe53ef2e8ade51b8d67ea3d5ac38889b5acf
AndriyPlekan <44374776+AndriyPlekan@users.noreply.github.com>
2020-11-18 11:43:06 +0200

    Add project files

---
+++ CarBackEnd/Car.Tests/UnitTest1.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using NUnit.Framework;
+
+namespace Car.Tests
+{
+    public class UnitTest1
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
---
+++ CarBackEnd/DAL/Interfaces/IRepository.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Car.DAL.Interfaces
+{
+    public interface IRepository<TEntity> where TEntity : IEntityBase
+    {
+    }
+}
