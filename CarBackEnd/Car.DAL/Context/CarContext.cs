9f0f5a915416e629d3bb4c71fdcb6d2cfa47852e
unknown <kotsun.nazar69@gmail.com>
2020-11-18 23:14:58 +0200

    Added car context and connection string

---
+++ CarBackEnd/Car.DAL/Context/CarContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Car.DAL.Context
+{
+    class CarContext : DbContext
+    {
+        public CarContext(DbContextOptions<CarContext> options) : base(options) { }
+    }
+}
