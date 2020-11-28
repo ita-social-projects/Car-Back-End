d1e4da7c3902d487cd20dfc538c3b00b1682ba27
Denbuvalyy <abcabcabcdenis@gmail.com>
2020-11-28 10:38:57 +0200

    Closes #85-add Swagger to project

--- CarBackEnd/CarBackEnd/Startup.cs
+++ CarBackEnd/CarBackEnd/Startup.cs
@@ -1,6 +1,8 @@
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Reflection;
 using System.Threading.Tasks;
 using CarBackEnd.ServiceExtension;
 using Microsoft.AspNetCore.Builder;
@@ -9,19 +11,24 @@ using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using Microsoft.OpenApi.Models;
+
 
 namespace CarBackEnd
 {
     public class Startup
     {
-        public Startup(IConfiguration configuration, IWebHostEnvironment environment)
+        public Startup(IConfiguration configuration, ILogger<Startup> logger, IWebHostEnvironment environment)
         {
             Configuration = configuration;
             Environment = environment;
+            _logger = logger;
         }
         // This method gets called by the runtime. Use this method to add services to the container.
         public IConfiguration Configuration { get; }
         private IWebHostEnvironment Environment { get; }
+        private readonly ILogger _logger;
 
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
@@ -29,6 +36,15 @@ namespace CarBackEnd
         {
             services.AddDbContext(Configuration, Environment);
             services.AddControllers();
+            services.AddLogging();
+            services.AddApplicationInsightsTelemetry();
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo { Title = "SoftServe Car-API", Version = "v1" });
+                var xmlFile = $"{Assembly.GetExecutingAssembly().GetName().Name}.xml";
+                var xmlPath = Path.Combine(AppContext.BaseDirectory, xmlFile);
+                c.IncludeXmlComments(xmlPath);
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -36,7 +52,12 @@ namespace CarBackEnd
         {
             if (env.IsDevelopment())
             {
+                _logger.LogInformation("Configuring for Development environment");
                 app.UseDeveloperExceptionPage();
+            }
+            else
+            {
+                _logger.LogInformation("Configuring for Production environment");
             }
 
             app.UseRouting();
@@ -44,6 +65,13 @@ namespace CarBackEnd
             app.UseEndpoints(endpoints =>
             {
                 endpoints.MapControllers();
+            });
+
+            app.UseSwagger();
+
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "SoftServe Car-API");
             });
         }
     }
