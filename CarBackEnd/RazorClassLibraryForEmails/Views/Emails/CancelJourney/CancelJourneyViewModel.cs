99c1a788c4315a3bcb5489675ab91e3f1a915859
bogdan_gavrylyak <gavrylyakbogdan@gmail.com>
2020-12-29 01:20:09 +0200

    changed-property-names

--- CarBackEnd/RazorClassLibraryForEmails/Views/Emails/CancelJourney/CancelJourneyViewModel.cs
+++ CarBackEnd/RazorClassLibraryForEmails/Views/Emails/CancelJourney/CancelJourneyViewModel.cs
@@ -6,7 +6,7 @@ namespace RazorClassLibraryForEmails.Views.Emails.CancelJourney
     public class CancelJourneyViewModel
     {
         public string DriverName { get; set; }
-        public DateTime TimeOfStoppage { get; set; }
+        public DateTime TimeOfCancellation { get; set; }
         public IFormFile Attachments { get; set; }
     }
 }
