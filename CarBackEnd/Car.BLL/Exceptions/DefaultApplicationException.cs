be9c2f5cbfe34fb3254feb3637c934d606a433d9
nazar-kotsun <47036004+nazar-kotsun@users.noreply.github.com>
2021-01-11 13:48:16 +0200

    Close #128 - Fixed bug with image disposing (#129)

--- CarBackEnd/Car.BLL/Exceptions/DefaultApplicationException.cs
+++ CarBackEnd/Car.BLL/Exceptions/DefaultApplicationException.cs
@@ -5,11 +5,15 @@ using Car.BLL.Dto;
 namespace Car.BLL.Exceptions
 {
     [Serializable]
-    public class DefaultApplicationException : Exception
+    public class DefaultApplicationException : Exception, ISerializable
     {
         public int StatusCode { get; set; }
 
         public Severity Severity { get; set; }
+
+        public DefaultApplicationException()
+        {
+        }
 
         public DefaultApplicationException(string message)
             : base(message)
