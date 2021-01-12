8e6dcfd7fafed44277fc5ff41ba2df53a7499d96
Roman Danylevych <roma.dan2001@gmail.com>
2021-01-12 22:45:16 +0200

    Covered Startup

---
+++ CarBackEnd/Car.Tests/TestStartup.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Data;
+using Car.BLL.Exceptions;
+using CarBackEnd;
+using Xunit;
+using FluentAssertions;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Car.Tests
+{
+    public class TestStartup
+    {
+        private Startup _startup;
+        private IServiceCollection _serviceCollection;
+        private IApplicationBuilder _applicationBuilder;
+        private IWebHostEnvironment _environment;
+
+
+        [Fact]
+        public void TestConfigureServices()
+        {
+            Action action = () => _startup.ConfigureServices(_serviceCollection);
+            action.Should().NotThrow<DefaultApplicationException>();
+        }
+
+        [Fact]
+        public void TestConfigure()
+        {
+            Action action = () => _startup.Configure(_applicationBuilder, _environment);
+            action.Should().NotThrow<DefaultApplicationException>();
+        }
+    }
+}
