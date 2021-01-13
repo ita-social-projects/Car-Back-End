165c107d46d61f71a15276aed98bc82ddaa84650
TetianaLysak <lysak.tetiana22@gmail.com>
2021-01-13 23:34:39 +0200

    Unique Nonclustered index on email field added

--- CarBackEnd/Car.DAL/Migrations/20210113212306_UserJourneyRelation.Designer.cs
+++ CarBackEnd/Car.DAL/Migrations/20210113212306_UserJourneyRelation.Designer.cs
@@ -4,14 +4,16 @@ using Car.DAL.Context;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace Car.DAL.Migrations
 {
     [DbContext(typeof(CarContext))]
-    partial class CarContextModelSnapshot : ModelSnapshot
+    [Migration("20210113212306_UserJourneyRelation")]
+    partial class UserJourneyRelation
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
@@ -220,7 +222,7 @@ namespace Car.DAL.Migrations
                         .UseIdentityColumn();
 
                     b.Property<string>("Email")
-                        .HasColumnType("nvarchar(450)");
+                        .HasColumnType("nvarchar(max)");
 
                     b.Property<DateTime>("HireDate")
                         .HasColumnType("datetime2");
@@ -241,10 +243,6 @@ namespace Car.DAL.Migrations
                         .HasColumnType("nvarchar(max)");
 
                     b.HasKey("Id");
-
-                    b.HasIndex("Email")
-                        .IsUnique()
-                        .HasFilter("[Email] IS NOT NULL");
 
                     b.ToTable("User");
                 });
