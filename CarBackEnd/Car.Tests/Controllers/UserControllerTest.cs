3dd36bbbb171b8aa2c4eb227906254bf81715538
Roman Alberda <ramzesball@gmail.com>
2021-01-15 00:37:14 +0200

    Added test UploadUserAvatar_WhenUserNotExist

--- CarBackEnd/Car.Tests/Controllers/UserControllerTest.cs
+++ CarBackEnd/Car.Tests/Controllers/UserControllerTest.cs
@@ -7,7 +7,11 @@ using CarBackEnd.Controllers;
 using FluentAssertions.Execution;
 using FluentAssertions;
 using Microsoft.AspNetCore.Mvc;
-using Car.BLL.Dto;
+using System.Threading.Tasks;
+using System;
+using Car.BLL.Exceptions;
+using Microsoft.AspNetCore.Http;
+using System.IO;
 
 namespace Car.Tests.Controllers
 {
@@ -16,6 +20,7 @@ namespace Car.Tests.Controllers
         private Mock<IUserService> _userService;
         private Mock<IImageService<User, File>> _imageService;
         private UserController _userController;
+
 
         public UserControllerTest()
         {
@@ -69,6 +74,34 @@ namespace Car.Tests.Controllers
                 result.Should().BeOfType<OkObjectResult>();
                 (result as OkObjectResult).Value.Should().BeNull();
             }
+        }
+
+        [Fact]
+        public void TestGetUserWithAvatarById_WithRightId_ReturnsOkObjectResult()
+        {
+            var user = GetTestUser();
+            _userService.Setup(u => u.GetUserWithAvatarById(user.Id)).Returns(user);
+
+            var result = _userController.GetUserWithAvatarById(user.Id);
+            using (new AssertionScope())
+            {
+                result.Should().BeOfType<OkObjectResult>();
+                (result as OkObjectResult).Value.Should().BeOfType<User>();
+                ((result as OkObjectResult).Value as User).Id.Should().Be(user.Id);
+                ((result as OkObjectResult).Value as User).Name.Should().Be(user.Name);
+                ((result as OkObjectResult).Value as User).Surname.Should().Be(user.Surname);
+                ((result as OkObjectResult).Value as User).Email.Should().Be(user.Email);
+                ((result as OkObjectResult).Value as User).Position.Should().Be(user.Position);
+            }
+        }
+
+        [Fact]
+        public async Task UploadUserAvatar_WhenUserNotExist()
+        {
+            var user = GetTestUser();
+
+            Func<Task> action = () => (Task)_imageService.Setup(p => p.UploadImage(user.Id, new FormFile(Stream.Null, 1, 1, "1", "1")));
+            await action.Should().NotThrowAsync<DefaultApplicationException>();
         }
     }
 }
---
+++ CarBackEnd/CarBackEndTests/Controllers/UserControllerTests.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CarBackEndTests.Controllers
+{
+    class UserControllerTests
+    {
+    }
+}
