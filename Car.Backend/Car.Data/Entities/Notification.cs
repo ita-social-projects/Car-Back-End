ba55782347b39c6ae2b533f124cfc38cdf28dd33
Roman Kovalchuk <37474734+Reennon@users.noreply.github.com>
2021-02-19 17:05:56 +0200

    Closes #238 notification async (#251)
    * Initial
    
    * fixed EF
    
    * Notification Fixed Routes + SignalR Amended
    
    * Last fix
    
    * Merge Conflicts fixed
    
    * Removed Tests
    
    * Fixed Code-Smells
    
    Co-authored-by: RomanKovalchuk <mrbylba@gmail.com>

--- Car.Backend/Car.Data/Entities/Notification.cs
+++ Car.Backend/Car.Data/Entities/Notification.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace Car.Data.Entities
 {
@@ -6,20 +7,20 @@ namespace Car.Data.Entities
     {
         public int Id { get; set; }
 
-        public int SenderId { get; set; }
-
-        public int ReceiverId { get; set; }
-
-        public string Data { get; set; }
-
-        public bool IsRead { get; set; }
-
-        public NotificationType Type { get; set; }
-
-        public DateTime CreatedAt { get; set; }
-
         public User Sender { get; set; }
 
         public User Receiver { get; set; }
+
+        public int ReceiverId { get; set; }
+
+        public int SenderId { get; set; }
+
+        public NotificationType Type { get; set; }
+
+        public string JsonData { get; set; }
+
+        public bool IsRead { get; set; }
+
+        public DateTime CreatedAt { get; set; }
     }
 }
--- Car.Backend/Car.Domain/Dto/NotificationDto.cs
+++ Car.Backend/Car.Domain/Dto/NotificationDto.cs
@@ -1,28 +1,15 @@
-﻿using System;
-using Car.Data.Entities;
+﻿using Car.Data.Entities;
 
 namespace Car.Domain.Dto
 {
     public class NotificationDto
     {
-        public int Id { get; set; }
-
-        public int UserId { get; set; }
-
-        public string UserName { get; set; }
-
-        public string Position { get; set; }
+        public int SenderId { get; set; }
 
         public int ReceiverId { get; set; }
 
-        public string Description { get; set; }
+        public NotificationType Type { get; set; }
 
-        public bool IsRead { get; set; }
-
-        public DateTime CreateAt { get; set; }
-
-        public int JourneyId { get; set; }
-
-        public NotificationType NotificationType { get; set; }
+        public string JsonData { get; set; }
     }
 }
