c08f98d51d13e8ef6636ce450f8144c6e9605dab
TetianaLysak <lysak.tetiana22@gmail.com>
2021-03-01 16:13:41 +0200

    Chat controller tests fixed

---
+++ Car.Backend/Car.UnitTests/Controllers/ChatControllerTest.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using AutoFixture;
+using Car.Data.Entities;
+using Car.Domain.Services.Interfaces;
+using Car.WebApi.Controllers;
+using FluentAssertions;
+using Microsoft.AspNetCore.Mvc;
+using Moq;
+using Xunit;
+
+namespace Car.UnitTests.Controllers
+{
+    public class ChatControllerTest
+    {
+        private readonly Mock<IChatService> chatService;
+        private readonly ChatController chatController;
+        private readonly Fixture fixture;
+
+        public ChatControllerTest()
+        {
+            chatService = new Mock<IChatService>();
+            chatController = new ChatController(chatService.Object);
+
+            fixture = new Fixture();
+
+            fixture.Behaviors.Remove(new ThrowingRecursionBehavior());
+            fixture.Behaviors.Add(new OmitOnRecursionBehavior());
+        }
+
+        [Fact]
+        public async Task GetUserChats_WhenUserExists_ReturnsChatObject()
+        {
+            // Arrange
+            var user = fixture.Create<User>();
+            var chats = fixture.Create<List<Chat>>();
+
+            chatService.Setup(x => x.GetUserChatsAsync(It.IsAny<int>()))
+                .ReturnsAsync(chats);
+
+            // Act
+            var result = await chatController.GetUserChats(user.Id);
+
+            // Assert
+            result.Should().BeOfType<OkObjectResult>();
+            (result as OkObjectResult)?.Value.Should().BeEquivalentTo(chats);
+        }
+
+        [Fact]
+        public async Task GetUserChats_WhenUserNotExists_ReturnsNull()
+        {
+            // Arrange
+            var user = fixture.Create<User>();
+            chatService.Setup(x => x.GetUserChatsAsync(It.IsAny<int>()))
+                .ReturnsAsync((List<Chat>)null);
+
+            // Act
+            var result = await chatController.GetUserChats(user.Id);
+
+            // Assert
+            result.Should().BeOfType<OkObjectResult>();
+            (result as OkObjectResult)?.Value.Should().BeNull();
+        }
+    }
+}
