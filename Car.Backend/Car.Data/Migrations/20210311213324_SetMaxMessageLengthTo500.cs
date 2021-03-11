29f75b729423fdfbc84f2ca676964bdddb0df00e
Roman Alberda <58659160+RamzesUAA@users.noreply.github.com>
2021-03-12 01:09:39 +0200

    Added max legth for messages (#319)

---
+++ Car.Backend/Car.Data/Migrations/20210311213324_SetMaxMessageLengthTo500.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Car.Data.Migrations
+{
+    public partial class SetMaxMessageLengthTo500 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Text",
+                table: "Message",
+                type: "nvarchar(500)",
+                maxLength: 500,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(400)",
+                oldMaxLength: 400);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Text",
+                table: "Message",
+                type: "nvarchar(400)",
+                maxLength: 400,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(500)",
+                oldMaxLength: 500);
+        }
+    }
+}
