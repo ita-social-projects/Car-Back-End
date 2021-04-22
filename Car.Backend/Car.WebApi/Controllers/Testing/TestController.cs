c6028da4701e8dc9e26bd57ce27bd84492cea547
Ilya Shevchyk <il2001sh@gmail.com>
2021-04-22 18:07:32 +0300

    for azure testing (will be deleted soon)

---
+++ Car.Backend/Car.WebApi/Controllers/Testing/TestController.cs
@@ -0,0 +1,17 @@
+using System.Threading.Tasks;
+using Car.Domain.Services.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Car.WebApi.Controllers
+{
+    [Route("api/testing")]
+    [ApiController]
+    public class TestController : ControllerBase
+    {
+        [HttpGet("{text}")]
+        public string Do(string text)
+        {
+            return text;
+        }
+    }
+}
