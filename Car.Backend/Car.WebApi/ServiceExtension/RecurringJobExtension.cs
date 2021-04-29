bd0caa0c2d385ba4680f6efd2d0681006b944412
roman-pankiv <64358780+roman-pankiv@users.noreply.github.com>
2021-04-29 14:56:17 +0300

    Recurring jobs configuring (#396)
    * initial configurations and chat-deleting background job
    
    * changed job name
    
    * provided removing of outdated journeys
    
    * code refactoring

---
+++ Car.Backend/Car.WebApi/ServiceExtension/RecurringJobExtension.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Car.Domain.Services.Interfaces;
+using Hangfire;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Car.WebApi.ServiceExtension
+{
+    public static class RecurringJobExtension
+    {
+        public static void AddReccuringJobs(this IServiceProvider serviceProvider, IRecurringJobManager recurringJobManager)
+        {
+            recurringJobManager.AddOrUpdate("DeleteOutdatedJourneys",
+                                            () => serviceProvider.GetService<IJourneyService>()
+                                                                 .DeletePastJourneyAsync(),
+                                            Cron.Daily(),
+                                            TimeZoneInfo.Local);
+        }
+    }
+}
