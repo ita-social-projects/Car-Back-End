2144219188d560ba268f9bf2663e825bd365260e
vovazavislyak <vowa222216@gmail.com>
2021-04-29 19:03:13 +0300

    Delete Address.City and Address.Street and added Address.Name

--- Car.Backend/Car.Data/Migrations/CarContextModelSnapshot.cs
+++ Car.Backend/Car.Data/Migrations/CarContextModelSnapshot.cs
@@ -26,21 +26,15 @@ namespace Car.Data.Migrations
                         .HasColumnType("int")
                         .HasAnnotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn);
 
-                    b.Property<string>("City")
-                        .IsRequired()
-                        .HasMaxLength(50)
-                        .HasColumnType("nvarchar(50)");
-
                     b.Property<double>("Latitude")
                         .HasColumnType("float");
 
                     b.Property<double>("Longitude")
                         .HasColumnType("float");
 
-                    b.Property<string>("Street")
+                    b.Property<string>("Name")
                         .IsRequired()
-                        .HasMaxLength(50)
-                        .HasColumnType("nvarchar(50)");
+                        .HasColumnType("nvarchar(max)");
 
                     b.HasKey("Id");
 
