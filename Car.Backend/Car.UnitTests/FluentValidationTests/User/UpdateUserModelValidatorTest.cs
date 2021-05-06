76444ce18b2691dfe7d9e230972b1900ca8bec6a
vovazavislyak <vowa222216@gmail.com>
2021-05-06 14:49:19 +0300

    Refactored FluentValidationTests folder

---
+++ Car.Backend/Car.UnitTests/FluentValidationTests/User/UpdateUserModelValidatorTest.cs
@@ -0,0 +1,34 @@
+﻿using Car.Domain.FluentValidation;
+using FluentValidation.TestHelper;
+using NUnit.Framework;
+using Xunit;
+
+namespace Car.UnitTests.FluentValidationTests.User
+{
+    [TestFixture]
+    public class UpdateUserModelValidatorTest
+    {
+        private readonly UpdateUserModelValidator validator;
+
+        public UpdateUserModelValidatorTest()
+        {
+            validator = new UpdateUserModelValidator();
+        }
+
+        [Xunit.Theory]
+        [InlineData(0)]
+        [InlineData(-1)]
+        public void Id_IsNotValid_GeneratesValidationError(int value)
+        {
+            validator.ShouldHaveValidationErrorFor(userModel => userModel.Id, value);
+        }
+
+        [Xunit.Theory]
+        [InlineData(1)]
+        [InlineData(10)]
+        public void Id_IsSpecified_NotGeneratesValidationError(int value)
+        {
+            validator.ShouldNotHaveValidationErrorFor(userModel => userModel.Id, value);
+        }
+    }
+}
