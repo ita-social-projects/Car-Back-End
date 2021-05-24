af5b20934fd50166e67a1c0868e79c2384a0e15e
andriikalymon <65017966+andriikalymon@users.noreply.github.com>
2021-05-24 19:44:21 +0300

    Update PreferencesController not ot use UserPreferences entity (#488)

---
+++ Car.Backend/Car.Domain/FluentValidation/PreferencesDtoValidator.cs
@@ -0,0 +1,16 @@
+﻿using Car.Data.Constants;
+using FluentValidation;
+
+namespace Car.Domain.FluentValidation
+{
+    public class PreferencesDtoValidator : AbstractValidator<Dto.UserPreferencesDTO>
+    {
+        public PreferencesDtoValidator()
+        {
+            RuleFor(userPreferences => userPreferences.Id).GreaterThan(Constants.IdLength);
+            RuleFor(userPreferences => userPreferences.DoAllowSmoking).NotNull();
+            RuleFor(userPreferences => userPreferences.DoAllowEating).NotNull();
+            RuleFor(userPreferences => userPreferences.Comments).MaximumLength(Constants.CommentsMaxLength);
+        }
+    }
+}
