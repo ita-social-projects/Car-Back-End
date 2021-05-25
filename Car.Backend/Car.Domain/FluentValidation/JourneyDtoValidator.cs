a5da4430e6cb928bbb97da33dbe2a8d5b8dff53e
vovazavislyak <vowa222216@gmail.com>
2021-05-25 20:18:20 +0300

    Added JourneyDtoValidator

---
+++ Car.Backend/Car.Domain/FluentValidation/JourneyDtoValidator.cs
@@ -0,0 +1,26 @@
+using System;
+using Car.Data.Constants;
+using Car.Domain.Dto;
+using FluentValidation;
+
+namespace Car.Domain.FluentValidation
+{
+    public class JourneyDtoValidator : AbstractValidator<JourneyDto>
+    {
+        public JourneyDtoValidator()
+        {
+            RuleFor(model => model.DepartureTime).GreaterThan(DateTime.UtcNow);
+            RuleFor(model => model.CountOfSeats)
+                .GreaterThan(Constants.NumberMin)
+                .LessThanOrEqualTo(Constants.SeatsMaxCount);
+            RuleFor(model => model.Comments).MaximumLength(Constants.CommentsMaxLength);
+            RuleFor(model => model.IsFree).NotNull();
+            RuleFor(model => model.OrganizerId).GreaterThan(Constants.IdLength);
+            RuleFor(model => model.IsOnOwnCar).NotNull();
+            RuleForEach(model => model.JourneyPoints).SetValidator(new JourneyPointDtoValidator());
+            RuleForEach(model => model.Stops).SetValidator(new StopDtoValidator());
+            RuleFor(model => model.RouteDistance).GreaterThan(Constants.NumberMin);
+            RuleFor(model => model.Duration).GreaterThan(TimeSpan.Zero);
+        }
+    }
+}
