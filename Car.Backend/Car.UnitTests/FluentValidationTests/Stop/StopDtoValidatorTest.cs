98f6d920d0c6aa70cf7c966acc18a98e0d8b29e9
vovazavislyak <vowa222216@gmail.com>
2021-06-02 20:30:26 +0300

    Added validation rules for Index and tests for them

--- Car.Backend/Car.UnitTests/FluentValidationTests/Stop/StopDtoValidatorTest.cs
+++ Car.Backend/Car.UnitTests/FluentValidationTests/Stop/StopDtoValidatorTest.cs
@@ -9,11 +9,27 @@ namespace Car.UnitTests.FluentValidationTests.Stop
     [TestFixture]
     public class StopDtoValidatorTest
     {
-        private StopDtoValidator validator;
+        private readonly StopDtoValidator validator;
 
         public StopDtoValidatorTest()
         {
             validator = new StopDtoValidator();
+        }
+
+        [Xunit.Theory]
+        [InlineData(-1)]
+        [InlineData(-10)]
+        public void Index_IsNotValid_GeneratesValidationError(int value)
+        {
+            validator.ShouldHaveValidationErrorFor(stopDto => stopDto.Index, value);
+        }
+
+        [Xunit.Theory]
+        [InlineData(1)]
+        [InlineData(10)]
+        public void Index_IsSpecified_NotGeneratesValidationError(int value)
+        {
+            validator.ShouldNotHaveValidationErrorFor(stopDto => stopDto.Index, value);
         }
 
         [Xunit.Theory]
