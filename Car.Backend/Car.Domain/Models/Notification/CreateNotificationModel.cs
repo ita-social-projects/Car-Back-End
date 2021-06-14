c1c1a05ca60d31f05a283e24f9da4b367337bb35
roman-pankiv <roman14ig@gmail.com>
2021-06-15 00:32:21 +0300

    changed CreateNotificationModel

--- Car.Backend/Car.Domain/Models/Notification/CreateNotificationModel.cs
+++ Car.Backend/Car.Domain/Models/Notification/CreateNotificationModel.cs
@@ -10,12 +10,8 @@ namespace Car.Domain.Models.Notification
 
         public NotificationType Type { get; set; }
 
-<<<<<<< HEAD
-        public string JsonData { get; set; }
+        public string JsonData { get; set; } = "{}";
 
         public int? JourneyId { get; set; }
-=======
-        public string JsonData { get; set; } = "{}";
->>>>>>> ee067aa6ef45f4d099c648db6f1b2d4c0d7c9133
     }
 }
