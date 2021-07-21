9571905fffaf26c0b403c2c9efecc8d370b188be
Yurii Danyliv <52209685+YuriiDanyliv@users.noreply.github.com>
2021-07-21 22:59:10 +0300

    Delete notification (#595)
    * add checking if chat exist
    
    * fix notification about journey cancellation
    
    * change delete range method

--- Car.Backend/Car.Domain/Services/Implementation/JourneyService.cs
+++ Car.Backend/Car.Domain/Services/Implementation/JourneyService.cs
@@ -180,8 +180,7 @@ namespace Car.Domain.Services.Implementation
                 journeyToCancel.DepartureTime = DateTime.UtcNow;
                 await journeyRepository.SaveChangesAsync();
 
-                var notificationsToDelete = mapper.Map<IEnumerable<Notification>, IEnumerable<NotificationDto>>(journeyToCancel.Notifications);
-                await notificationService.DeleteNotificationsAsync(notificationsToDelete);
+                await notificationService.DeleteNotificationsAsync(journeyToCancel.Notifications);
                 await notificationService.NotifyParticipantsAboutCancellationAsync(journeyToCancel);
             }
         }
