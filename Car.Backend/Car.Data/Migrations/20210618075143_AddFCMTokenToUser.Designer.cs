cba50437f53b94ff32b3fba94f1a5d5e490c754e
Vasyl Shtenovych <vasyl.shtenovych@gmail.com>
2021-09-01 19:53:04 +0300

    Add missing migration, missing column to model snapshot

--- Car.Backend/Car.Data/Migrations/20210618075143_AddFCMTokenToUser.Designer.cs
+++ Car.Backend/Car.Data/Migrations/20210618075143_AddFCMTokenToUser.Designer.cs
@@ -4,14 +4,16 @@ using Car.Data.Context;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace Car.Data.Migrations
 {
     [DbContext(typeof(CarContext))]
-    partial class CarContextModelSnapshot : ModelSnapshot
+    [Migration("20210618075143_AddFCMTokenToUser")]
+    partial class AddFCMTokenToUser
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
@@ -442,9 +444,6 @@ namespace Car.Data.Migrations
                     b.Property<TimeSpan>("Duration")
                         .HasColumnType("time");
 
-                    b.Property<bool>("IsCancelled")
-                        .HasColumnType("bit");
-
                     b.Property<bool>("IsFree")
                         .HasColumnType("bit");
 
@@ -492,24 +491,6 @@ namespace Car.Data.Migrations
                     b.ToTable("JourneyPoints");
                 });
 
-            modelBuilder.Entity("Car.Data.Entities.JourneyUser", b =>
-                {
-                    b.Property<int>("JourneyId")
-                        .HasColumnType("int");
-
-                    b.Property<int>("UserId")
-                        .HasColumnType("int");
-
-                    b.Property<bool>("WithBaggage")
-                        .HasColumnType("bit");
-
-                    b.HasKey("JourneyId", "UserId");
-
-                    b.HasIndex("UserId");
-
-                    b.ToTable("JourneyUser");
-                });
-
             modelBuilder.Entity("Car.Data.Entities.Location", b =>
                 {
                     b.Property<int>("Id")
@@ -522,7 +503,8 @@ namespace Car.Data.Migrations
 
                     b.Property<string>("Name")
                         .IsRequired()
-                        .HasColumnType("nvarchar(max)");
+                        .HasMaxLength(50)
+                        .HasColumnType("nvarchar(50)");
 
                     b.Property<int>("TypeId")
                         .HasColumnType("int");
@@ -571,9 +553,6 @@ namespace Car.Data.Migrations
 
                     b.Property<DateTime>("CreatedAt")
                         .HasColumnType("datetime2");
-
-                    b.Property<bool>("IsRead")
-                        .HasColumnType("bit");
 
                     b.Property<int>("SenderId")
                         .HasColumnType("int");
@@ -8099,9 +8078,6 @@ namespace Car.Data.Migrations
                     b.Property<bool>("IsRead")
                         .HasColumnType("bit");
 
-                    b.Property<int?>("JourneyId")
-                        .HasColumnType("int");
-
                     b.Property<string>("JsonData")
                         .IsRequired()
                         .HasMaxLength(1000)
@@ -8118,31 +8094,11 @@ namespace Car.Data.Migrations
 
                     b.HasKey("Id");
 
-                    b.HasIndex("JourneyId");
-
                     b.HasIndex("ReceiverId");
 
                     b.HasIndex("SenderId");
 
                     b.ToTable("Notification");
-                });
-
-            modelBuilder.Entity("Car.Data.Entities.ReceivedMessages", b =>
-                {
-                    b.Property<int>("ChatId")
-                        .HasColumnType("int");
-
-                    b.Property<int>("UserId")
-                        .HasColumnType("int");
-
-                    b.Property<int>("UnreadMessagesCount")
-                        .HasColumnType("int");
-
-                    b.HasKey("ChatId", "UserId");
-
-                    b.HasIndex("UserId");
-
-                    b.ToTable("ReceivedMessages");
                 });
 
             modelBuilder.Entity("Car.Data.Entities.Request", b =>
@@ -8198,9 +8154,6 @@ namespace Car.Data.Migrations
 
                     b.Property<int>("Index")
                         .HasColumnType("int");
-
-                    b.Property<bool>("IsCancelled")
-                        .HasColumnType("bit");
 
                     b.Property<int>("JourneyId")
                         .HasColumnType("int");
@@ -8259,9 +8212,6 @@ namespace Car.Data.Migrations
                         .HasMaxLength(64)
                         .HasColumnType("nvarchar(64)");
 
-                    b.Property<string>("PhoneNumber")
-                        .HasColumnType("nvarchar(max)");
-
                     b.Property<string>("Position")
                         .HasMaxLength(100)
                         .HasColumnType("nvarchar(100)");
@@ -8297,6 +8247,21 @@ namespace Car.Data.Migrations
                     b.HasKey("Id");
 
                     b.ToTable("UserPreferences");
+                });
+
+            modelBuilder.Entity("JourneyUser", b =>
+                {
+                    b.Property<int>("ParticipantJourneysId")
+                        .HasColumnType("int");
+
+                    b.Property<int>("ParticipantsId")
+                        .HasColumnType("int");
+
+                    b.HasKey("ParticipantJourneysId", "ParticipantsId");
+
+                    b.HasIndex("ParticipantsId");
+
+                    b.ToTable("JourneyUser");
                 });
 
             modelBuilder.Entity("Car.Data.Entities.Car", b =>
@@ -8358,25 +8323,6 @@ namespace Car.Data.Migrations
                     b.Navigation("Journey");
                 });
 
-            modelBuilder.Entity("Car.Data.Entities.JourneyUser", b =>
-                {
-                    b.HasOne("Car.Data.Entities.Journey", "Journey")
-                        .WithMany("JourneyUsers")
-                        .HasForeignKey("JourneyId")
-                        .OnDelete(DeleteBehavior.Cascade)
-                        .IsRequired();
-
-                    b.HasOne("Car.Data.Entities.User", "User")
-                        .WithMany("JourneyUsers")
-                        .HasForeignKey("UserId")
-                        .OnDelete(DeleteBehavior.Cascade)
-                        .IsRequired();
-
-                    b.Navigation("Journey");
-
-                    b.Navigation("User");
-                });
-
             modelBuilder.Entity("Car.Data.Entities.Location", b =>
                 {
                     b.HasOne("Car.Data.Entities.Address", "Address")
@@ -8436,11 +8382,6 @@ namespace Car.Data.Migrations
 
             modelBuilder.Entity("Car.Data.Entities.Notification", b =>
                 {
-                    b.HasOne("Car.Data.Entities.Journey", "Journey")
-                        .WithMany("Notifications")
-                        .HasForeignKey("JourneyId")
-                        .OnDelete(DeleteBehavior.Cascade);
-
                     b.HasOne("Car.Data.Entities.User", "Receiver")
                         .WithMany("ReceivedNotifications")
                         .HasForeignKey("ReceiverId")
@@ -8453,30 +8394,9 @@ namespace Car.Data.Migrations
                         .OnDelete(DeleteBehavior.Restrict)
                         .IsRequired();
 
-                    b.Navigation("Journey");
-
                     b.Navigation("Receiver");
 
                     b.Navigation("Sender");
-                });
-
-            modelBuilder.Entity("Car.Data.Entities.ReceivedMessages", b =>
-                {
-                    b.HasOne("Car.Data.Entities.Chat", "Chat")
-                        .WithMany("ReceivedMessages")
-                        .HasForeignKey("ChatId")
-                        .OnDelete(DeleteBehavior.Cascade)
-                        .IsRequired();
-
-                    b.HasOne("Car.Data.Entities.User", "User")
-                        .WithMany("ReceivedMessages")
-                        .HasForeignKey("UserId")
-                        .OnDelete(DeleteBehavior.Cascade)
-                        .IsRequired();
-
-                    b.Navigation("Chat");
-
-                    b.Navigation("User");
                 });
 
             modelBuilder.Entity("Car.Data.Entities.Request", b =>
@@ -8589,6 +8509,21 @@ namespace Car.Data.Migrations
                     b.Navigation("User");
                 });
 
+            modelBuilder.Entity("JourneyUser", b =>
+                {
+                    b.HasOne("Car.Data.Entities.Journey", null)
+                        .WithMany()
+                        .HasForeignKey("ParticipantJourneysId")
+                        .OnDelete(DeleteBehavior.Cascade)
+                        .IsRequired();
+
+                    b.HasOne("Car.Data.Entities.User", null)
+                        .WithMany()
+                        .HasForeignKey("ParticipantsId")
+                        .OnDelete(DeleteBehavior.Cascade)
+                        .IsRequired();
+                });
+
             modelBuilder.Entity("Car.Data.Entities.Address", b =>
                 {
                     b.Navigation("Location");
@@ -8609,8 +8544,6 @@ namespace Car.Data.Migrations
             modelBuilder.Entity("Car.Data.Entities.Chat", b =>
                 {
                     b.Navigation("Messages");
-
-                    b.Navigation("ReceivedMessages");
                 });
 
             modelBuilder.Entity("Car.Data.Entities.Journey", b =>
@@ -8618,10 +8551,6 @@ namespace Car.Data.Migrations
                     b.Navigation("Chat");
 
                     b.Navigation("JourneyPoints");
-
-                    b.Navigation("JourneyUsers");
-
-                    b.Navigation("Notifications");
 
                     b.Navigation("Schedule");
 
@@ -8642,13 +8571,9 @@ namespace Car.Data.Migrations
                 {
                     b.Navigation("Cars");
 
-                    b.Navigation("JourneyUsers");
-
                     b.Navigation("Locations");
 
                     b.Navigation("OrganizerJourneys");
-
-                    b.Navigation("ReceivedMessages");
 
                     b.Navigation("ReceivedNotifications");
 
