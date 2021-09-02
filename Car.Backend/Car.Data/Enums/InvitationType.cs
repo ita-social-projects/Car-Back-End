73463299bae1324bd09ba80402b4a691c69c8415
Vasyl Shtenovych <40914946+shteniovych7@users.noreply.github.com>
2021-09-02 11:45:42 +0300

    Add invitation entity, migrations (#668)

---
+++ Car.Backend/Car.Data/Enums/InvitationType.cs
@@ -0,0 +1,20 @@
+﻿namespace Car.Data.Enums
+{
+    public enum InvitationType
+    {
+        /// <summary>
+        /// Invintation is pending.
+        /// </summary>
+        Sent,
+
+        /// <summary>
+        /// Passenger accepted invitation.
+        /// </summary>
+        Accepted,
+
+        /// <summary>
+        /// Passenger rejected invitation.
+        /// </summary>
+        Rejected,
+    }
+}
