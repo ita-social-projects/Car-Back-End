049900a720021124690155a9ab9adf6138397d5c
Volodymyr Ruban <Tulen2001@gmail.com>
2021-09-15 16:14:01 +0300

    Show unread messages in chat (#701)
    * Changed add chat method
    
    * Added ReveivedMessagesController | Added ReceivedMessagesServiceService | AddChatAsync in ChatServices changed implementation
    
    * Added GetReceivedMessagesFromChat to ReceivedMessagesService | Added tests | Fixed code smell
    
    * Update ReceivedMessagesControllerTest.cs
    
    Removed blank line
    
    * ReceivedMessagesService gets userId from httpContextAccessor
    
    * Added methods to controller | Added service methdos | Fixed service method
    
    * Added ChatServiceExtensions |  Added Dto | Modified GetUserChatsAsync method | Removed excessive tests | Modified old ones
    
    * Removed unused method
    
    * Fixed code smells
    
    * fixed code smells

--- Car.Backend/Car.Domain/Services/Implementation/ReceivedMessagesService.cs
+++ Car.Backend/Car.Domain/Services/Implementation/ReceivedMessagesService.cs
@@ -1,7 +1,11 @@
-﻿using System.Threading.Tasks;
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
 using Car.Data.Entities;
 using Car.Data.Infrastructure;
 using Car.Domain.Services.Interfaces;
+using Car.WebApi.ServiceExtension;
+using Microsoft.AspNetCore.Http;
 using Microsoft.EntityFrameworkCore;
 
 namespace Car.Domain.Services.Implementation
@@ -9,15 +13,20 @@ namespace Car.Domain.Services.Implementation
     public class ReceivedMessagesService : IReceivedMessagesService
     {
         private readonly IRepository<ReceivedMessages> receivedMessagesRepository;
+        private readonly IHttpContextAccessor httpContextAccessor;
 
         public ReceivedMessagesService(
-            IRepository<ReceivedMessages> receivedMessagesRepository)
+            IRepository<ReceivedMessages> receivedMessagesRepository,
+            IHttpContextAccessor httpContextAccessor)
         {
             this.receivedMessagesRepository = receivedMessagesRepository;
+            this.httpContextAccessor = httpContextAccessor;
         }
 
-        public async Task<int> MarkMessagesReadInChatAsync(int userId, int chatId)
+        public async Task<int> MarkMessagesReadInChatAsync(int chatId)
         {
+            int userId = httpContextAccessor.HttpContext!.User.GetCurrentUserId();
+
             var receivedMessages = await receivedMessagesRepository.Query()
                 .FirstOrDefaultAsync(rm => rm.ChatId == chatId && rm.UserId == userId)!;
             receivedMessages.UnreadMessagesCount = 0;
@@ -26,8 +35,10 @@ namespace Car.Domain.Services.Implementation
             return receivedMessages.UnreadMessagesCount;
         }
 
-        public async Task<int> GetUnreadMessageForChatAsync(int userId, int chatId)
+        public async Task<int> GetUnreadMessageForChatAsync(int chatId)
         {
+            int userId = httpContextAccessor.HttpContext!.User.GetCurrentUserId();
+
             var receivedMessages = await receivedMessagesRepository.Query()
                 .FirstOrDefaultAsync(rm => rm.ChatId == chatId && rm.UserId == userId)!;
 
--- Car.Backend/Car.Domain/Services/Interfaces/IReceivedMessagesService.cs
+++ Car.Backend/Car.Domain/Services/Interfaces/IReceivedMessagesService.cs
@@ -1,15 +1,13 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
+﻿using System.Collections.Generic;
 using System.Threading.Tasks;
+using Car.Data.Entities;
 
 namespace Car.Domain.Services.Interfaces
 {
     public interface IReceivedMessagesService
     {
-        Task<int> MarkMessagesReadInChatAsync(int userId, int chatId);
+        Task<int> MarkMessagesReadInChatAsync(int chatId);
 
-        Task<int> GetUnreadMessageForChatAsync(int userId, int chatId);
+        Task<int> GetUnreadMessageForChatAsync(int chatId);
     }
 }
